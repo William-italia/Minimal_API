b3099029b93985e63e42a857d26d5bae83c8b55b
william-italia <williamitalia70@outlook.com>
2022-02-26 14:22:50 -0300

    minimal_api

--- Program.cs
+++ Program.cs
@@ -7,18 +7,43 @@ builder.Services.AddEndpointsApiExplorer();
 // CONFIGURAÇÕES
 var app = builder.Build();
 
+//middleware
+
+app.UseHttpsRedirection();
+
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
-// //ADICIONAR SERVIÇOS
-app.MapGet("/", () => DateTime.Now);
 
-app.MapGet("/teste1", () =>
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+// //ADICIONAR SERVIÇOS
+// app.MapGet("/", () => DateTime.Now);
+
+app.MapGet("/api", () => "Resposta ao método GET");
+app.MapPost("/api", () => "Resposta ao método POST");
+app.MapPut("/api", () => "Resposta ao método PUT");
+app.MapDelete("/api", () => "Resposta ao método DELETE");
+app.MapMethods("/api", new[] { "PATCH" }, () => "Resposta ao método PATCH");
+// app.MapGet("/api", (int x, int y) => $"Recebido: x={x} e y+{y}"); Query string
+// app.MapGet("/api/{x}/{y}", (int x, int y) => $"Recebido: x={x} e y={y}");
+// app.MapGet("/api/{x}", ([FromRoute] int x, [FromQuery] int y) => $"Recebido: x={x} e y={y}");
+
+app.MapPost("/apixy", ([FromBody] DadosEntrada entrada) => $"Recebido: x = {entrada.x} e y ={entrada.y}");
+
+// app.MapGet("/teste1", () =>
+// {
+
+//   var agora = DateTime.Now;
+//   return $"Agora é {agora}.";
+// });
+
+app.MapGet("/hellow-word", () =>
 {
 
-  var agora = DateTime.Now;
-  return $"Agora é {agora}.";
+  return Results.Ok(new { mensagem = "Hellow, word!" });
 });
 
 app.MapGet("/soma", (int n1, int n2) =>
@@ -50,7 +75,15 @@ app.MapPost("/triangulo", ([FromBody] area area) =>
   return Results.Ok($"{(area.x * area.y):N3}");
 });
 
+app.Logger.LogInformation("Aplicação iniciada.");
 
 app.Run();
 
 public record area(double x, double y);
+
+public class DadosEntrada
+{
+  public int x { get; set; }
+
+  public int y { get; set; }
+}
